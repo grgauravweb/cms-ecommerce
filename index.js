1764d40c1a962f8e47df4f916b8906c9838be014
Gaurav Rai <55633403+grgauravweb@users.noreply.github.com>
2024-04-08 00:36:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const express = require('express');
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const route = require('./routes/route.js');
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
-require('dotenv').config();
+const dotenv = require("dotenv").config();
+const PORT = 5000;
+const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
+const brandRouter = require("./routes/brandRoute");
+const colorRouter = require("./routes/colorRoute");
+const enqRouter = require("./routes/enqRoute");
+const couponRouter = require("./routes/couponRoute");
+const uploadRouter = require("./routes/uploadRoute");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 
+dbConnect();
+app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/api/user", authRouter);
+app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
+app.use("/api/brand", brandRouter);
+app.use("/api/coupon", couponRouter);
+app.use("/api/color", colorRouter);
+app.use("/api/enquiry", enqRouter);
+app.use("/api/upload", uploadRouter);
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-mongoose.connect(process.env.mongo_uri)
-
-.then( () => console.log("Mongo is connected"))
-
-.catch(err => console.log(err))
-
-app.use('/', route); 
-
-
-app.listen(process.env.PORT || 9999, function () {
-
-    console.log(`CMS Server running on port ` + (process.env.PORT || 9999 ));
-    
-})
+// app.use(notFound);
+// app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`Server is running  at PORT ${PORT}`);
+});
